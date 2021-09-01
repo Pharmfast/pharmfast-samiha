985b02e5d461bea1d4b102c7fafa5bf49c1e257d
Sakib Noman <skbnmn@gmail.com>
2021-09-01 21:03:55 +0600

    test item button

--- src/components/LabTest/AllTestsComponent.js
+++ src/components/LabTest/AllTestsComponent.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import Drawer from "react-modern-drawer";
 import 'react-modern-drawer/dist/index.css';
 import { useDispatch } from 'react-redux';
 
-function AllTestsComponent({ fetchLab,_id, name, availablelab, price, toggleDrawer, isOpen, enterAlltest, removeFromCart }) {
+function AllTestsComponent({ fetchLab, _id, name, availablelab, price, toggleDrawer, isOpen, enterAlltest, removeFromCart }) {
     const dispatch = useDispatch();
 
     const selectAllTests = () => {
-        if(_id) {
+        if (_id) {
             dispatch(enterAlltest({
                 alltestId: _id
             }));
         }
     }
-    
+
     return (
         <div className=" 
         cursor-pointer shadow-xl rounded-lg mt-6 m-1 from-red-100 bg-gradient-to-b xl:from-white xl:bg-gradient-to-b
@@ -26,10 +25,12 @@ function AllTestsComponent({ fetchLab,_id, name, availablelab, price, toggleDraw
                 <h1 className="hidden xl:flex mt-16 pt-3 font-semibold text-gray-400 text-sm xl:mt-0 xl:font-semibold">Available at{availablelab} Certified Lab</h1>
                 <h1 className="hidden xl:flex mt-16 pt-3 font-semibold text-gray-600 text-sm xl:mt-0 xl:font-semibold xl:space-x-2">₹{price} <span className="text-gray-400 space-x-2">onwards</span></h1>
             </div>
-            <button onClick={() => fetchLab(_id)} 
-             className="xl:p-2 xl:pl-9 xl:pr-9 xl:bg-red-500 xl:rounded-lg xl:outline-none xl:text-white ">Select</button>
-             <button onClick={() => removeFromCart(_id)} 
-             className="xl:p-2 xl:pl-9 xl:pr-9 xl:bg-red-500 xl:rounded-lg xl:outline-none xl:text-white ">Remove</button>
+            <div className="flex flex-col" >
+                <button onClick={() => fetchLab(_id)}
+                    className="xl:p-2 xl:pl-9 xl:pr-9 xl:bg-red-500 xl:rounded-lg xl:outline-none xl:text-white ">Select</button>
+                <button onClick={() => removeFromCart(_id)}
+                    className="xl:p-2 xl:pl-9 xl:pr-9 xl:bg-red-500 xl:rounded-lg xl:outline-none xl:text-white mt-5 ">Remove</button>
+            </div>
         </div>
     )
 }
